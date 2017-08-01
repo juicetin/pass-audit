ef3f468261ae34973ae4f11884bb609f181889e0
Justin Ting <justingling@gmail.com>
2017-08-01 18:11:42 +1000

    run password checks with prompts at appropriate steps to update as required

--- audit.py
+++ audit.py
@@ -1,6 +1,7 @@
-from subprocess import check_output, CalledProcessError
+from subprocess import check_output, CalledProcessError, run
 import os
 import pdb
+import sys
 
 PASS_PATH='/home/me/.password-store'
 MIN_LENGTH=15
@@ -39,12 +40,50 @@ def pass_is_insecure(output):
     # TODO run through list of check functions
     return not sufficient_length(pw, MIN_LENGTH)
 
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+            "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                    "(or 'y' or 'n').\n")
+
 def execute_pass_for_file(cur_file):
     try:
         print('Checking {}...'.format(cur_file)) # 
         output = check_output(['pass', cur_file])
         if pass_is_insecure(output):
             print_insecure_file(cur_file)
+            modify_current = query_yes_no('Ready to modify current password? Current one will copy to clipboard. No to skip.', default="no")
+            if modify_current is True:
+                # TODO only run if flag checked
+                # TODO spawn browser with contained url as user has chosen to change pw
+                run(['pass', 'update', '-c', '-l', '30', cur_file]) 
+                query_yes_no('Ready to continue with file check?', default="no")
             return True
         else:
             return False
